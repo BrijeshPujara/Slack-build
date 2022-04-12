31dcd338bbef43efcbe6d5c19b33c6b182a9e6fa
Brijesh Pujara <pujara.brijesh1@gmail.com>
2022-04-12 18:50:14 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div className="app">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Header/>}></Route>
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBgiEA7LxLhVRXjB4nzZvW5YibMHeATPCA",
+    authDomain: "slack-build-2595b.firebaseapp.com",
+    projectId: "slack-build-2595b",
+    storageBucket: "slack-build-2595b.appspot.com",
+    messagingSenderId: "1084439311187",
+    appId: "1:1084439311187:web:944d6a76bbc529b4af67a6",
+    measurementId: "G-CC2643957T"
+  };

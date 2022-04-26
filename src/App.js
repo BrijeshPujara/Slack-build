10809b5babb35bf20fddedf2ab3e4f1db15a26f0
Brijesh Pujara <pujara.brijesh1@gmail.com>
2022-04-26 19:11:25 +0100

    google authentication added

--- src/App.js
+++ src/App.js
@@ -4,18 +4,25 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Header from "./components/Header";
 import styled from "styled-components";
 import Sidebar from "./components/Sidebar";
+import Chat from "./components/Chat";
+import { auth } from "./firebase";
+import {useAuthState} from "react-firebase-hooks/auth"
+
 
 function App() {
+  const [user, loading] = useAuthState(auth);
+
   return (
     <div className="app">
       <Router>
-        <Routes>
-          <Route path="/" element={<Header />}>
-            {/* Chat */}
-          </Route>
-        </Routes>
+        <Header />
         <AppBody>
           <Sidebar />
+          <Routes>
+          <Route path="/" element={<Chat />}>
+            
+          </Route>
+        </Routes>
         </AppBody>
       </Router>
     </div>

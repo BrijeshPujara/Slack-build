0495b96b2a0d8de717a0dca9c895f27cd5ccf63f
Brijesh Pujara <pujara.brijesh1@gmail.com>
2022-04-26 14:56:42 +0100

    firebase package installation

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,6 @@
+import firebase from "firebase";
+
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyBgiEA7LxLhVRXjB4nzZvW5YibMHeATPCA",
@@ -7,4 +10,11 @@ const firebaseConfig = {
     messagingSenderId: "1084439311187",
     appId: "1:1084439311187:web:944d6a76bbc529b4af67a6",
     measurementId: "G-CC2643957T"
-  };
+};
+  
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider, db };
